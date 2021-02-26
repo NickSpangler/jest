c5baade0ee591109928a4779f49dfce3424bd17f
NickSpangler <Spangleyman@mac.com>
2021-02-26 16:31:51 -0500

    add test

--- jest-testing/src/tests/App.test.tsx
+++ jest-testing/src/tests/App.test.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { App } from '../App'
+import Todos from '../Todos'
+
+test('renders the correct content', () => {
+   //Render a React component to the DOM
+   const root = document.createElement('div');
+   ReactDOM.render(<Todos />, root);
+
+   expect(root.querySelector('h1').textContent).toBe('TODOs')
+})
 
